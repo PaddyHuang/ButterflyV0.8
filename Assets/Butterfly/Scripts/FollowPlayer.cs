4766ba9298ee59c6dbdb698c6911d59470654bff
PaddyH <p19596@163.com>
2018-11-17 12:23:39 +0800

    Upload Butterfly

---
+++ Assets/Butterfly/Scripts/FollowPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform follower;
+	public Transform spectator;
+	public float smoothing = 3;
+    private Transform followerTrasnform;
+    private Transform spectatorTransform;
+    private Transform myTransform;
+		// 相机缓冲速度
+	
+	void Start(){
+        followerTrasnform = follower;
+        spectatorTransform = spectator;
+        myTransform=transform;
+	}
+	
+	void Update(){
+        transform.position = Vector3.Lerp(myTransform.position, followerTrasnform.position, smoothing * Time.deltaTime);
+        transform.LookAt(spectatorTransform.position);
+
+	}
+		
+
+
+
+
+
+}
+
---
+++ Assets/Butterfly/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private string steel="Steel";
+    public AudioClip butterflyShock;
+    private AudioSource audio;
+
+
+    void Start() 
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+	void OnCollisionEnter(Collision collision)
+	{
+		
+        if (collision.collider.CompareTag(steel))  
+		{
+            audio.clip = butterflyShock;
+            audio.Play();
+			Destroy(collision.collider.gameObject);
+		}
+	}
+}
